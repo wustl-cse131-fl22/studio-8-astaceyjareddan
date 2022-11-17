5cea2716d258f56e69bf5776cf170762b2ef0454
staceykang <staceykang@staceys-mbp.dhcp.wustl.edu>
2022-11-17 14:11:17 -0600

    studio 8

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,9 +1,41 @@
 package studio8;
 
 public class Date {
+	
+	private int month;
+	private int day;
+	private int year;
+	private boolean isHoliday;
+	
+	/**
+	 * constructor
+	 * @param initmonth
+	 * @param initday
+	 * @param inityear
+	 * @param initholiday
+	 */
+	public Date(int initmonth, int initday, int inityear, boolean initholiday) {
+		month = initmonth;
+		day = initday;
+		year = inityear;
+		isHoliday = initholiday;
+		
+	}
+	
+	
 
 
-    public static void main(String[] args) {
+    @Override
+	public String toString() {
+		return "Date [month=" + month + ", day=" + day + ", year=" + year + ", holiday=" + isHoliday + "]";
+	}
+
+
+
+
+	public static void main(String[] args) {
+		Date a = new Date(12,25,1999,true);
+		Date b = new Date(11,22,1986,false);
 
     }
 
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,8 +2,38 @@ package studio8;
 
 
 public class Time {
+	
+	private int hour;
+	private int minute;
+	private boolean twelveHourFormat;
+	
+	/**
+	 * constructor
+	 * @param initHour
+	 * @param initMinute
+	 * @param initHourFormat
+	 */
+	public Time(int initHour, int initMinute, boolean  initHourFormat ) {
+		hour = initHour;
+		minute = initMinute;
+		twelveHourFormat = initHourFormat;
+		
+		
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Time [hour=" + hour + ", minute=" + minute + ", hourFormat=" + twelveHourFormat + "]";
+	}
+
+
 
 	public static void main(String[] args) {
+		Time a = new Time(7,55,true);
+		Time b = new Time(16,43,false);
+		
  
     	
     }
